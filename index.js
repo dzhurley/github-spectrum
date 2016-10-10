2ca646928af3e7db5f72dd05905844f326bec402
Derek Hurley <dzhurley@gmail.com>
2016-10-10 16:35:58 -0700

    don't assume browser supports default parameters

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var color2color = require('colorcolor');
 var spectrum = require('./spectrum.json');
 
-module.exports = function(language, type='hex', calculateOpacity=false) {
+module.exports = function(language, type, calculateOpacity) {
     return language in spectrum ?
-        color2color(spectrum[language], type, calculateOpacity) :
+        color2color(
+            spectrum[language],
+            type || 'hex',
+            calculateOpacity || false
+        ) :
         '';
 };
